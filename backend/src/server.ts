03ba2ad70f3c0163ab0022aa7d2ebd72782da9ee
EmmanuelMose <emmanuelmose806@gmail.com>
2025-12-26 09:02:36 +0300

    26 commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import app from "./index";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
